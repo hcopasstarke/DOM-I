e2f4ece85190e9633dd07bc3fea727550a7acf94
hcopasstarke <hcopasstarke@gmail.com>
2019-05-20 16:03:42 -0700

    attempted

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,5 @@
+let secondTens = document.querySelector("secondTens");
+let secondOnes = document.querySelector("secondOnes");
+let colon = document.querySelector("colon");
+let msHundreds = document.querySelector("msHundreds");
+let msTens = document.querySelector("msTens");
